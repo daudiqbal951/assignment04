ad2e450e8f32c0771ac243e96d6afd0941c4c07d
daudiqbal951 <daudiqbal951@yahoo.com>
2023-08-20 14:25:44 +0500

    file updated

--- task01.js
+++ task01.js
@@ -1,8 +1,9 @@
 "use strict";
 //Create a function that takes an array, an index, and a value as parameters. Inside the function,
 //use the splice method to insert the value at the specified index in the array Return the modified 
+var testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function addvalue(array, index, value) {
     array.splice(index, 0, value);
     return array;
 }
-console.log(addvalue([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 8));
+console.log(addvalue(testArray, 3, 8));
--- task01.ts
+++ task01.ts
@@ -1,8 +1,11 @@
 //Create a function that takes an array, an index, and a value as parameters. Inside the function,
 //use the splice method to insert the value at the specified index in the array Return the modified 
 
+var testArray:number[]=[1,2,3,4,5,6,7,8,9]
+
 function addvalue(array:number[],index:number,value:number){
     array.splice(index,0,value)
     return array
 }
-console.log(addvalue([1,2,3,4,5,6,7,8,9],3,8))
+
+console.log(addvalue(testArray,3,8))
