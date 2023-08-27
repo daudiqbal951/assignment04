677a4c9526f405bdfe0a1f752650da5aee9b95b1
daudiqbal951 <daudiqbal951@yahoo.com>
2023-08-27 09:56:14 +0500

    Updated

--- task06.ts
+++ task06.ts
@@ -1,12 +1,12 @@
 //Write a program having an array of numbers if the number is negative it should
 //remove the negative number from the array
 
-var numberArray:number[]=[-5,5,-3,3,-1,1,0]
+var numberArray:number[]=[-5,-5,-3,3,-1,1,0]
 var index:number=0
 while(index<numberArray.length){
     if(numberArray[index]<0){
         numberArray.splice(index,1)
-        i--
+        index--
     }
     else{
         index=index+1
